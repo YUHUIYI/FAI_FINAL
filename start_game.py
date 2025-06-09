eab489efcf3cb18d7afc2c58e2cea75105839a92
游輝毅 <b12705016@g.ntu.edu.tw>
2025-06-09 16:07:06 +0800

    Update start_game.py

--- start_game.py
+++ start_game.py
@@ -4,11 +4,12 @@ from agents.call_player import setup_ai as call_ai
 from agents.random_player import setup_ai as random_ai
 from agents.console_player import setup_ai as console_ai
 from baseline0 import setup_ai as baseline0_ai
+from baseline1 import setup_ai as baseline1_ai
 from agents.Heuristic_player import setup_ai as heuristic_ai
 from agents.RL_player import setup_ai as RL_ai
 
 config = setup_config(max_round=20, initial_stack=1000, small_blind_amount=5)
-config.register_player(name="p1", algorithm=baseline0_ai())
+config.register_player(name="p1", algorithm=baseline1_ai())
 config.register_player(name="p2", algorithm=RL_ai())
 
 ## Play in interactive mode if uncomment
